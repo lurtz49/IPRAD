cfc5eac56c8b778b53944906d48d74fe56141888
Ozan Doğan <ozandogan4949@gmail.com>
2017-12-06 04:23:13 +0300

    Add files via upload

---
+++ IPRAD/IPRAD/IPRAD/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace IPRAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.Location = new System.Drawing.Point(27, 12);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(370, 333);
+            this.picOriginal.TabIndex = 0;
+            this.picOriginal.TabStop = false;
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(452, 12);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(370, 333);
+            this.picResult.TabIndex = 1;
+            this.picResult.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(178, 373);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 44);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(595, 373);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(100, 44);
+            this.btnGray.TabIndex = 3;
+            this.btnGray.Text = "Gray";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 457);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.picOriginal);
+            this.Name = "Form1";
+            this.Text = "IPRAD";
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGray;
+
+    }
+}
+
---
+++ IPRAD/IPRAD/IPRAD/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IPRAD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter="Image File (*.bmp,jpg)| *.bmg;*.jpg";
+            if (DialogResult.OK==ofile.ShowDialog())
+            {
+                this.picOriginal.Image = new Bitmap(ofile.FileName);
+
+            }
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            Bitmap copy = new Bitmap(this.picOriginal.Image);
+
+            Processing.ConvertToGray(copy);
+            this.picResult.Image = copy;
+        }
+    }
+}
